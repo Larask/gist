2c48442cebf87c787639a48f9ebaf491c9bce123
Luu Hoang Nam <hoangnam0705@icloud.com>
2015-03-15 21:45:26 +0700

    Temporary not use versioning for css files

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>@yield('title', 'Larask Gist')</title>
 
-	<link href="{{ elixir('css/all.css') }}" rel="stylesheet">
+	<link href="{{ asset('css/all.css') }}" rel="stylesheet">
 
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
